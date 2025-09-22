f1e61051dd7551053b2af2346199b154bb380ca3
Arfa <arfaizzarayhani@gmail.com>
2025-09-22 20:46:48 +0700

    commit

--- AppPembalik.java
+++ AppPembalik.java
@@ -6,30 +6,21 @@ public class AppPembalik {
     public static void main(String[] args) {
         //output a
         String kataHardcode = "kasur";
-
         System.out.println(">> katanya...");
         System.out.println(kataHardcode);
-
         pembalik p1 = new pembalik(kataHardcode);
         String hasil1 = p1.balik();
-
         System.out.println(">> dibalik jadi...");
         System.out.println(hasil1);
         System.out.println(); 
-        
         Scanner sc = new Scanner(System.in);
         String kata;
-
         while (true) {
             System.out.print("Masukkan kata: ");
-            kata = sc.nextLine();
-
-          
+            kata = sc.nextLine();       
             if (kata.equals("")) break;
-
             pembalik p = new pembalik(kata);
             String hasil = p.balik();
-
             System.out.println("Kebalikan: " + hasil);
         }
         sc.close();
--- pembalik.java
+++ pembalik.java
@@ -1,28 +1,20 @@
 package strukturdataprak;
 public class pembalik {
     private String input;
-
-   
+    
     public pembalik(String input) {
         this.input = input;
-    }
-
-    
+    }   
     public String balik() {
         int panjang = input.length();
-        stack stack = new stack(panjang);
-
-        
+        stack stack = new stack(panjang);     
         for (int i = 0; i < panjang; i++) {
             stack.push(input.charAt(i));
-        }
-
-        
+        }  
         StringBuilder hasil = new StringBuilder();
         while (!stack.isEmpty()) {
             hasil.append(stack.pop());
         }
-
         return hasil.toString();
     }
 }
