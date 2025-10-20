9a3fe759aff33892843f2f4458145da6bed3e1c8
Arfa <arfaizzarayhani@gmail.com>
2025-10-21 06:42:33 +0700

    commit

--- TreeApp.java
+++ TreeApp.java
@@ -1,9 +1,8 @@
 package strukturdataprak;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class TreeApp {
+
     public static void main(String[] args) throws IOException {
         int value;
         String data;
@@ -19,7 +18,6 @@ public class TreeApp {
         theTree.insert(87, "Orin");
         theTree.insert(93, "Wiwid");
         theTree.insert(97, "Sasa");
-
         while (true) {
             System.out.print("Enter first letter of show, " + "insert, find, delete, or traverse: ");
             int choice = getChar();
@@ -28,7 +26,8 @@ public class TreeApp {
                     theTree.displayTree();
                     break;
                 case 'i':
-                    System.out.print("Enter value and data to" + " insert: ");
+                    System.out.print("Enter value and data to"
+                            + " insert: ");
                     value = getInt();
                     data = getString();
                     theTree.insert(value, data);
@@ -42,17 +41,18 @@ public class TreeApp {
                         found.displayNode();
                         System.out.print("\n");
                     } else {
-                        System.out.println("Could not find " + value);
+                        System.out.println("Could not find "
+                                + value);
                     }
                     break;
-                    case 'd':
+                case 'd':
                     System.out.print("Enter value to delete: ");
                     value = getInt();
                     boolean didDelete = theTree.delete(value);
                     if (didDelete) {
-                        System.out.println("Deleted " + value );
+                        System.out.println("Deleted " + value);
                     } else {
-                        System.out.println("Could not delete "  + value);
+                        System.out.println("Could not delete " + value);
                     }
                     break;
                 case 't':
@@ -65,16 +65,19 @@ public class TreeApp {
             }
         }
     }
+
     public static String getString() throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         String s = br.readLine();
         return s;
     }
+
     public static char getChar() throws IOException {
         String s = getString();
         return s.charAt(0);
     }
+
     public static int getInt() throws IOException {
         String s = getString();
         return Integer.parseInt(s);
