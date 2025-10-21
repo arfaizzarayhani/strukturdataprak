a6360ffa199bd5f270b7a5e8ff07417f7c460a35
Arfa <arfaizzarayhani@gmail.com>
2025-10-21 07:25:23 +0700

    modul 7

--- Tree.java
+++ Tree.java
@@ -1,61 +1,79 @@
 package strukturdataprak;
 import java.util.Stack;
+
 public class Tree {
-
     private Node root;
-
     public Tree() {
         root = null;
     }
-
     public Node find(int key) {
         Node current = root;
-        while (current.id != key) {
+        while (current != null && current.id != key) {
             if (key < current.id) {
                 current = current.leftChild;
             } else {
                 current = current.rightChild;
             }
-            if (current == null) {
-                return null;
-            }
         }
         return current;
     }
-
+    // MODIFIKASI INSERT
     public void insert(int id, String data) {
+        // Cek apakah key sudah ada
+        if (find(id) != null) {
+            System.out.println("Duplicate key not allowed: " + id);
+            return;
+        }
         Node newNode = new Node();
         newNode.id = id;
         newNode.data = data;
         if (root == null) {
             root = newNode;
-        } else {
-            Node current = root;
-            Node parent;
-            while (true) {
-                parent = current;
-                if (id < current.id) {
-                    current = current.leftChild;
-                    if (current == null) {
-                        parent.leftChild = newNode;
-                        return;
-                    }
-                } else {
-                    current = current.rightChild;
-                    if (current == null) {
-                        parent.rightChild = newNode;
-                        return;
-                    }
+            return;
+        }
+        Node current = root;
+        Node parent;
+        while (true) {
+            parent = current;
+            if (id < current.id) {
+                current = current.leftChild;
+                if (current == null) {
+                    parent.leftChild = newNode;
+                    return;
+                }
+            } else {
+                current = current.rightChild;
+                if (current == null) {
+                    parent.rightChild = newNode;
+                    return;
                 }
             }
         }
     }
+    // METHOD MENDAPATKAN MINIMUM DAN MAKSIMUM
+    public Node getMinimum() {
+        Node current = root;
+        if (current == null) return null;
 
+        while (current.leftChild != null) {
+            current = current.leftChild;
+        }
+        return current;
+    }
+    public Node getMaximum() {
+        Node current = root;
+        if (current == null) return null;
+
+        while (current.rightChild != null) {
+            current = current.rightChild;
+        }
+        return current;
+    }
     public boolean delete(int key) {
         Node current = root;
         Node parent = root;
         boolean isLeftChild = true;
-        while (current.id != key) {
+        while (current != null && current.id != key) {
             parent = current;
             if (key < current.id) {
                 isLeftChild = true;
@@ -64,49 +82,38 @@ public class Tree {
                 isLeftChild = false;
                 current = current.rightChild;
             }
-            if (current == null) {
-                return false;
-            }
         }
-        if (current.leftChild == null
-                && current.rightChild == null) {
-            if (current == root) {
-                root = null;
-            } else if (isLeftChild) {
-                parent.leftChild = current.leftChild;
-            } else {
-                parent.rightChild = current.leftChild;
-            }
-        } else if (current.rightChild == null) {
-            if (current == root) {
-                root = current.leftChild;
-            } else if (isLeftChild) {
-                parent.leftChild = current.leftChild;
-            } else {
-                parent.rightChild = current.leftChild;
-            }
-        } else if (current.leftChild == null) {
-            if (current == root) {
-                root = current.rightChild;
-            } else if (isLeftChild) {
-                parent.leftChild = current.rightChild;
-            } else {
-                parent.rightChild = current.rightChild;
-            }
-        } else {
+        if (current == null) {
+            return false;
+        }
+        // Case 1: Node tanpa child
+        if (current.leftChild == null && current.rightChild == null) {
+            if (current == root) root = null;
+            else if (isLeftChild) parent.leftChild = null;
+            else parent.rightChild = null;
+        }
+        // Case 2: Node hanya punya left child
+        else if (current.rightChild == null) {
+            if (current == root) root = current.leftChild;
+            else if (isLeftChild) parent.leftChild = current.leftChild;
+            else parent.rightChild = current.leftChild;
+        }
+        // Case 3: Node hanya punya right child
+        else if (current.leftChild == null) {
+            if (current == root) root = current.rightChild;
+            else if (isLeftChild) parent.leftChild = current.rightChild;
+            else parent.rightChild = current.rightChild;
+        }
+        // Case 4: Node punya dua child
+        else {
             Node successor = getSuccessor(current);
-            if (current == root) {
-                root = successor;
-            } else if (isLeftChild) {
-                parent.leftChild = successor;
-            } else {
-                parent.rightChild = successor;
-            }
+            if (current == root) root = successor;
+            else if (isLeftChild) parent.leftChild = successor;
+            else parent.rightChild = successor;
             successor.leftChild = current.leftChild;
         }
         return true;
     }
-
     private Node getSuccessor(Node delNode) {
         Node successorParent = delNode;
         Node successor = delNode;
@@ -122,7 +129,6 @@ public class Tree {
         }
         return successor;
     }
-
     public void traverse(int traverseType) {
         switch (traverseType) {
             case 1:
@@ -140,7 +146,6 @@ public class Tree {
         }
         System.out.println();
     }
-
     private void preOrder(Node localRoot) {
         if (localRoot != null) {
             System.out.print(localRoot.id + " ");
@@ -148,7 +153,6 @@ public class Tree {
             preOrder(localRoot.rightChild);
         }
     }
-
     private void inOrder(Node localRoot) {
         if (localRoot != null) {
             inOrder(localRoot.leftChild);
@@ -156,7 +160,6 @@ public class Tree {
             inOrder(localRoot.rightChild);
         }
     }
-
     private void postOrder(Node localRoot) {
         if (localRoot != null) {
             postOrder(localRoot.leftChild);
@@ -164,46 +167,38 @@ public class Tree {
             System.out.print(localRoot.id + " ");
         }
     }
-
     public void displayTree() {
         Stack globalStack = new Stack();
         globalStack.push(root);
         int nBlanks = 32;
         boolean isRowEmpty = false;
-        System.out.println(
-                "............................................");
-        while (isRowEmpty == false) {
+        System.out.println("............................................");
+        while (!isRowEmpty) {
             Stack localStack = new Stack();
             isRowEmpty = true;
-            for (int j = 0; j < nBlanks; j++) {
+            for (int j = 0; j < nBlanks; j++)
                 System.out.print(' ');
-            }
-            while (globalStack.isEmpty() == false) {
+            while (!globalStack.isEmpty()) {
                 Node temp = (Node) globalStack.pop();
                 if (temp != null) {
                     System.out.print(temp.id);
                     localStack.push(temp.leftChild);
                     localStack.push(temp.rightChild);
-                    if (temp.leftChild != null
-                            || temp.rightChild != null) {
+                    if (temp.leftChild != null || temp.rightChild != null)
                         isRowEmpty = false;
-                    }
                 } else {
                     System.out.print("--");
                     localStack.push(null);
                     localStack.push(null);
                 }
-                for (int j = 0; j < nBlanks * 2 - 2; j++) {
+                for (int j = 0; j < nBlanks * 2 - 2; j++)
                     System.out.print(' ');
-                }
             }
             System.out.println();
             nBlanks /= 2;
-            while (localStack.isEmpty() == false) {
+            while (!localStack.isEmpty())
                 globalStack.push(localStack.pop());
-            }
         }
-        System.out.println(
-                "............................................");
+        System.out.println("............................................");
     }
 }
--- TreeApp.java
+++ TreeApp.java
@@ -2,11 +2,11 @@ package strukturdataprak;
 import java.io.*;
 
 public class TreeApp {
-
     public static void main(String[] args) throws IOException {
         int value;
         String data;
         Tree theTree = new Tree();
+
         theTree.insert(50, "Ahmad");
         theTree.insert(25, "Rosa");
         theTree.insert(75, "Raisa");
@@ -18,16 +18,22 @@ public class TreeApp {
         theTree.insert(87, "Orin");
         theTree.insert(93, "Wiwid");
         theTree.insert(97, "Sasa");
+        //theTree.insert(55, "Carlos");
+
         while (true) {
-            System.out.print("Enter first letter of show, " + "insert, find, delete, or traverse: ");
+            System.out.print("""
+                             
+                             Enter first letter of:
+                             show, insert, find, delete, traverse,
+                             or (m) for Min/Max: """);
             int choice = getChar();
+
             switch (choice) {
                 case 's':
                     theTree.displayTree();
                     break;
                 case 'i':
-                    System.out.print("Enter value and data to"
-                            + " insert: ");
+                    System.out.print("Enter value and data to insert: ");
                     value = getInt();
                     data = getString();
                     theTree.insert(value, data);
@@ -41,8 +47,7 @@ public class TreeApp {
                         found.displayNode();
                         System.out.print("\n");
                     } else {
-                        System.out.println("Could not find "
-                                + value);
+                        System.out.println("Could not find " + value);
                     }
                     break;
                 case 'd':
@@ -56,28 +61,38 @@ public class TreeApp {
                     }
                     break;
                 case 't':
-                    System.out.print("Enter type 1, 2 or 3: ");
+                    System.out.print("Enter type 1, 2 or 3 (Pre, In, Post order): ");
                     value = getInt();
                     theTree.traverse(value);
                     break;
+
+                // Tambahan fitur untuk menampilkan nilai min & max
+                case 'm':
+                    Node minNode = theTree.getMinimum();
+                    Node maxNode = theTree.getMaximum();
+                    if (minNode != null && maxNode != null) {
+                        System.out.println("Minimum value in tree: " + minNode.id + " (" + minNode.data + ")");
+                        System.out.println("Maximum value in tree: " + maxNode.id + " (" + maxNode.data + ")");
+                    } else {
+                        System.out.println("Tree is empty!");
+                    }
+                    break;
+
                 default:
                     System.out.println("Invalid entry ");
             }
         }
     }
-
     public static String getString() throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         String s = br.readLine();
         return s;
     }
-
     public static char getChar() throws IOException {
         String s = getString();
         return s.charAt(0);
     }
-
     public static int getInt() throws IOException {
         String s = getString();
         return Integer.parseInt(s);
