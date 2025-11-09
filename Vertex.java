dcd23fb26cda914e40a66be095ef07d1b141f83a
Arfa <arfaizzarayhani@gmail.com>
2025-11-09 10:18:19 +0700

    modul 10

---
+++ Vertex.java
@@ -0,0 +1,11 @@
+package strukturdataprak;
+public class Vertex {
+
+    public char label;
+    public boolean wasVisited;
+
+    public Vertex(char label) {
+        this.label = label;
+        wasVisited = false;
+    }
+} 
